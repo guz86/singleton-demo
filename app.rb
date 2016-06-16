caa5ae9fe5d3920a81e59799267b9e69e428df95
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-16 20:36:27 +0300

    updae logger instance method

--- app.rb
+++ app.rb
@@ -3,10 +3,23 @@ class Logger
     @f = File.open 'log.txt','a'
   end
 
+@@x = nil
+
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+
+    return @@x
+  end
+
+
+  # class method
   def self.say_something
     puts 'ha'
   end
 
+  # instance method
   def log_something what  
     @f.puts what
   end
@@ -15,6 +28,8 @@ end
 
 #logger.say_something
 Logger.say_something
+Logger.instance.log_something 'bla bla'
+Logger.instance.log_something 'bla bla2'
 
-logger = Logger.new
-logger.log_something 'aaa'
+#logger = Logger.new
+#logger.log_something 'aaa'
