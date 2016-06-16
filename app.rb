7f7656abb549a971c784e2ae0e8b126fa3645633
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-16 20:18:38 +0300

    add method log

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ class Logger
   def self.say_something
     puts 'ha'
   end
+
+  def self.log_something what
+    f = File.open 'log.txt','a'
+    f.puts what
+    f.close
+  end
 end
 
 #logger = Logger.new
 #logger.say_something
 Logger.say_something
+Logger.log_something 'waaaa'
